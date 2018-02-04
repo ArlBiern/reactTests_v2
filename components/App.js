ae468cade5976f1cd3cde76acd1a0af094ea7dfd
Ariel Biernacki <arielbiernacki@gmail.com>
2018-02-04 18:35:04 +0100

    Start of project

---
+++ components/App.js
@@ -0,0 +1,37 @@
+var contacts = [
+	{
+		id: 1,
+		firstName: 'Jan',
+		lastName: 'Kowalski',
+		email: 'jan.kowalski@example.com'
+	},
+	{
+		id: 2,
+		firstName: 'Paweł',
+		lastName: 'Musi',
+		email: 'pawel.musi@example.com'
+	},
+	{
+		id: 3,
+		firstName: 'Adam',
+		lastName: 'Nowak',
+		email: 'adam.nowak@example.com'
+	},
+];
+
+var contactForm = {
+	firstName: '',
+	lastName: '',
+	email: ''
+};
+
+var App = React.createClass({
+	render: function() {
+		return (
+			React.createElement('div', {className: 'app'},
+				React.createElement(ContactForm, {contact: contactForm}),
+				React.createElement(Contacts, {items: contacts}, {})
+			)
+		)
+	}
+});
